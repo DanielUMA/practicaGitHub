a09621d44b2b9e35b53c40fb109102379981597a
DanielUMA <danielhs17@uma.es>
2018-05-22 19:23:05 +0200

    Creo archivo RamonDani.java

---
+++ RamonDani.java
@@ -0,0 +1,18 @@
+public class RamonDani {
+
+	private int coolness;
+	
+	public RamonDani(int num) {
+		coolness = num*100000;
+	}
+	
+	public void ramon() {
+		System.out.println("Mimimi");
+	}
+
+	public void dani() {
+		System.out.println("¡Campeon!");
+	}
+
+
+}
